e1a5a4ffa5db3ce97c9eabfd01911f21cb0fc5db
Guilherme Rialli Oliveira <gui.rialli@gmail.com>
2024-01-23 14:18:41 -0300

    Arrumando algumas cambiarras de codigo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const PDFParser  = require("pdf2json");
+const PDFParser = require("pdf2json");
 const fs = require('fs/promises');
 const path = require('path');
 const { existsSync } = require('fs');
@@ -14,6 +14,9 @@ async function main() {
     const files_pdf = await fs.readdir(PDF_DIR)
     arquivos = files_pdf.length;
 
+    if (arquivos == 0)
+        throw new Error("Sem PDFs na pasta!")
+
     files_pdf.forEach(async (pdf) => {
         const json_name = path.basename(pdf).split(".")[0] + ".json";
         const path_final_json = path.join(JSON_DIR, json_name);
@@ -26,7 +29,7 @@ async function main() {
             const regexTelefone = /(\d{2})[\D\-]?\d{5}[\D\-]?\d{4}/;
             const regexDinheiro = /(\d{2})%2C(\d{2})/;
             const regexDinheiroCentena = /(\d{3})%2C(\d{2})/;
-            const regexDinheiroNova = /(\d{1}.(\d{3})%2C(\d{2}))/;
+            const regexDinheiroMilhar = /(\d{1}.(\d{3})%2C(\d{2}))/;
 
             const telNumeros = []
             const dinheiro = []
@@ -34,49 +37,50 @@ async function main() {
 
             Object.values(pdfData["Pages"]).forEach(pages => {
                 Object.values(pages["Texts"]).forEach(linhas => {
-                    Object.values(linhas["R"]).forEach((r, index, arr) => {
+                    Object.values(linhas["R"]).forEach((r) => {
                         const linhaT = String(r["T"]);
 
-                        if(regexTelefone.test(linhaT) && linhaT.includes("%20")){
+                        if (regexTelefone.test(linhaT) && linhaT.includes("%20")) {
                             const l = linhaT.split("%20").join("")
-                            if(l.length == 13 && !telNumeros.includes(l)){
+                            if (l.length == 13 && !telNumeros.includes(l)) {
                                 telNumeros.push(l)
                             }
                         }
-                        else{
-                            if(linhaT.includes("TOTAL%20PARA%20CADA%20CELULAR"))liberar=6
-                            if((regexDinheiro.test(linhaT) || regexDinheiroCentena.test(linhaT) || regexDinheiroNova.test(linhaT)) && (linhaT.length >= 7 && linhaT.length <= 10) && liberar > 0){
+                        else {
+                            if (linhaT.includes("TOTAL%20PARA%20CADA%20CELULAR")) liberar = 6
+                            else if ((regexDinheiro.test(linhaT) || regexDinheiroCentena.test(linhaT) || regexDinheiroMilhar.test(linhaT))
+                                && (linhaT.length >= 7 && linhaT.length <= 10)
+                                && liberar > 0) {
                                 const formatedMoney = linhaT.split("%2C").join(",")
                                 dinheiro.push(formatedMoney)
-                                liberar -=1;
+                                liberar -= 1;
                             }
                         }
                     })
                 })
             })
-            const obj = []
             
-            for(let i = 0; i < telNumeros.length; i++){
+            const obj = []
+            for (let i = 0; i < telNumeros.length; i++) {
                 obj.push({
-                    numero : telNumeros[i],
+                    numero: telNumeros[i],
                     dinheiro: dinheiro[i]
                 })
             }
             await fs.writeFile(path_final_json, JSON.stringify(obj), "utf-8");
             arquivos -= 1
         });
-       
+
         pdfParser.loadPDF(path.join(PDF_DIR, pdf))
     })
 }
 
-const  checarSeAcabou = setInterval( async () => {
-    if(arquivos  === 0){
+const checarSeAcabou = setInterval(async () => {
+    if (arquivos === 0) {
         clearInterval(checarSeAcabou)
         const arquivos_json = await fs.readdir(JSON_DIR);
-        const diretorio_xls = path.join(OUT_DIR, "arquivo_final.xlsx")
 
-        for (i in arquivos_json){
+        for (i in arquivos_json) {
             const arquivo = arquivos_json[i]
             const diretorio_json = path.join(JSON_DIR, arquivo)
             const diretorio_xls = path.join(OUT_DIR, arquivo.split(".")[0] + ".xlsx")
@@ -84,10 +88,7 @@ const  checarSeAcabou = setInterval( async () => {
             const buffer = jsonrawtoxlsx(file)
             await fs.writeFile(diretorio_xls, buffer, "binary")
         }
-
-
     }
 }, 1000)
-
 
 main();
